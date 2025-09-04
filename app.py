4a292d73231c0f668197786eb4a0fdbec9e51868
JavierAstrain <j.astrain.import@gmail.com>
2025-09-04 13:40:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
+from utils.md import df_to_md
 
 from utils.gsheets import load_sheets
 from utils.skills import (
